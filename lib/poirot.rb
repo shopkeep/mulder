ea3f60bb1a11b7720dd2f46210c085f843d678f0
Duncan Grazier <itsmeduncan@gmail.com>
2013-09-16 14:00:49 -0400

    Poirot::Capistrano can return ip address for the AutoScalingGroup

---
+++ lib/poirot.rb
@@ -0,0 +1,9 @@
+module Poirot
+
+  require_relative 'poirot/client'
+  require_relative 'poirot/config'
+  require_relative 'poirot/connection'
+  require_relative 'poirot/instance'
+  require_relative 'poirot/version'
+
+end
---
+++ lib/poirot/connection.rb
@@ -0,0 +1,44 @@
+require 'fog'
+
+module Poirot
+  class Connection
+
+    attr_reader :config
+
+    def initialize(config)
+      @config = config
+    end
+
+    def group_by_id_regexp(id_regexp)
+      autoscaler.groups.find_all do |group|
+        group.id =~ id_regexp
+      end.last
+    end
+
+    def instances_by_group(group)
+      group.instances.collect do |instance|
+        compute_instance = server_by_instance_id(instance.id)
+        Poirot::Instance.new(compute_instance)
+      end
+    end
+
+    def server_by_instance_id(instance_id)
+      compute.servers.get(instance_id)
+    end
+
+    def autoscaler
+      @autoscaler ||= ::Fog::AWS::AutoScaling.new(
+        :aws_access_key_id => @config.aws_access_key_id,
+        :aws_secret_access_key => @config.aws_secret_access_key
+      )
+    end
+
+    def compute
+      @compute ||= ::Fog::Compute::AWS.new(
+        :aws_access_key_id => @config.aws_access_key_id,
+        :aws_secret_access_key => @config.aws_secret_access_key
+      )
+    end
+
+  end
+end
